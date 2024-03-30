18539305152e0cb3da975d2e2c4a5c1709944fa7
Chandra Halim <chan.halim98@gmail.com>
2024-03-30 12:11:41 -0700

    f

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MahasiswaModule } from './mahasiswa/mahasiswa.module';
+import { DosenController } from './dosen/dosen.controller';
+import { DosenService } from './dosen/dosen.service';
+import { DosenModule } from './dosen/dosen.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [MahasiswaModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), MahasiswaModule, DosenModule],
+  controllers: [DosenController],
+  providers: [DosenService],
 })
 export class AppModule {}
---
+++ src/dosen/dosen.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('dosen')
+export class DosenController {}
